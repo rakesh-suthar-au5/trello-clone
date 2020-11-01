4a03a6a9e9f84b86e16a3cc73d136a2fc9fd59ab
DevRakeshSuthar <dev.rakeshsuthar@gmail.com>
2020-11-01 09:28:44 +0530

    FPRT

---
+++ trelloapp/src/Reducers/index.js
@@ -0,0 +1,18 @@
+import {combineReducers, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import {createStore } from 'redux';
+import stateReducers from './stateReducers'
+import authReducers from './authReducers'
+import boardReducer from './boardReducer';
+import errorReducer from './errorReducers'
+const rootReducers = combineReducers({
+    lists:stateReducers,
+    auth:authReducers,
+    board:boardReducer,
+    error:errorReducer
+})
+
+
+
+const store = createStore(rootReducers,applyMiddleware(thunk))
+export default store
---
+++ trelloapp/src/actionCreators/errorActions.js
@@ -0,0 +1,13 @@
+export const returnErrors = (msg, status, id = null) => {
+  // console.log(msg,status)
+  return {
+    type: "GET_ERRORS",
+    payload: { msg, status, id },
+  };
+};
+
+export const clearErrors = () => {
+  return {
+    type: "CLEAR_ERRORS",
+  };
+};
